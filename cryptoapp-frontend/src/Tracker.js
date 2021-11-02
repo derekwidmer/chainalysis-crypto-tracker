83f97b938f07d34f3e8d39e9cf1dcf8b68f339bd
Derek Widmer <dwidmer99@gmail.com>
2021-11-02 16:32:36 -0400

    Consume api with axios

--- cryptoapp-frontend/src/Tracker.js
+++ cryptoapp-frontend/src/Tracker.js
@@ -1,8 +1,21 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import CryptoCard from './CryptoCard'
 import { bitcoin, ethereum } from '../src/icons'
+import axios from 'axios';
 
 export default function Tracker(props) {
+
+	const axiosInstance = axios.create({
+		baseURL: "http://127.0.0.1:8080/api/price",
+		headers: { 'Content-Type': 'application/json' }
+	});
+
+	useEffect(() => {
+		axiosInstance.get().then(res => {
+			console.log(res)
+		}).catch(error => console.log(error))
+	})
+
 	return (
 		<div className="tracker d-flex justify-content-center align-items-center">
 			<div className="row">
