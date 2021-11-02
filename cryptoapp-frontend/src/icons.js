2385e6507ac0d10b9d7f5494a6d23650ceb74a7f
Derek Widmer <dwidmer99@gmail.com>
2021-11-01 22:51:06 -0400

    Fix icon name

--- cryptoapp-frontend/src/icons.js
+++ cryptoapp-frontend/src/icons.js
@@ -1,2 +1,2 @@
 export let bitcoin = "https://img.icons8.com/ios-filled/150/000000/bitcoin.png";
-export let etherium = "https://img.icons8.com/ios-filled/150/000000/ethereum.png";
+export let ethereum = "https://img.icons8.com/ios-filled/150/000000/ethereum.png";
