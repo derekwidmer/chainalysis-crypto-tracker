d307f1ce34bda54bbc48d443377dbadc4eb55369
Derek Widmer <dwidmer99@gmail.com>
2021-11-01 23:28:44 -0400

    Add icons

--- cryptoapp-frontend/src/CryptoCard.js
+++ cryptoapp-frontend/src/CryptoCard.js
@@ -5,8 +5,10 @@ export default function CryptoCard(props) {
 		<div className=" col-12 col-md-6 p-3 d-flex justify-content-center align-items-center">
 			<div className="card crypto-card">
 				<div className="card-body upper-body">
-					<h5 className="card-title crypto-title px-1">{props.ticker}</h5>
-					<img src={props.icon}/>
+					<h5 className="card-title crypto-title px-2 d-flex align-items-center">
+						{props.ticker}
+						<img className="icon ms-auto" alt="svgImg" src={props.icon} />
+					</h5>
 				</div>
 				<div className="card-body">
 					<h5 className="card-text px-1 pb-1">Price:</h5>
